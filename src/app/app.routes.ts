b80360c04539468d24fcb328682d8556b5a64fe9
Pepito <pepito@myre.fr>
2025-04-17 16:50:50 +0200

    add rounded user

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,16 +4,23 @@ import { paymentDetailsCanActivateFn } from './payments/payment-details/payment-
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'payments',
+    loadComponent: () => import('./main/main.component').then(m => m.MainComponent),
+    children: [
+      {
+        path: '',
+        redirectTo: 'payments',
+        pathMatch: 'full',
+      },
+      {
+        path: 'payments',
+        loadComponent: () => import('./payments/payments-list/payments-list.component').then(m => m.PaymentsListComponent),
+      },
+      {
+        path: 'payments/:paymentId',
+        canActivate: [paymentDetailsCanActivateFn],
+        loadComponent: () => import('./payments/payment-details/payment-details.component').then(m => m.PaymentDetailsComponent),
+      },
+    ],
     pathMatch: 'full',
   },
-  {
-    path: 'payments',
-    loadComponent: () => import('./payments/payments-list/payments-list.component').then(m => m.PaymentsListComponent),
-  },
-  {
-    path: 'payments/:paymentId',
-    canActivate: [paymentDetailsCanActivateFn],
-    loadComponent: () => import('./payments/payment-details/payment-details.component').then(m => m.PaymentDetailsComponent),
-  }
 ];
---
+++ src/app/main/main.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatIcon } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-main',
+  imports: [
+    RouterOutlet,
+    MatIcon,
+  ],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.scss'
+})
+export class MainComponent {
+
+}
