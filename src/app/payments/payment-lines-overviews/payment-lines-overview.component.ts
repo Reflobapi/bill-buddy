e12bf2cd8bcef66b31b3847d3ddee7ffb4c9804d
Reflobapi <clayette.pierre@gmail.com>
2025-04-26 14:37:01 +0200

    improve data structure

--- src/app/payments/payment-lines-overviews/payment-lines-overview.component.ts
+++ src/app/payments/payment-lines-overviews/payment-lines-overview.component.ts
@@ -1,4 +1,10 @@
-import { ChangeDetectionStrategy, Component, computed, input, signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  input,
+  signal,
+} from '@angular/core';
 import { GetPaymentLinesOverviewWithName } from '../payment-details/interfaces';
 import { FinancialValueComponent } from '../../lib/financial-value/financial-value.component';
 import { ProgressBarComponent } from '../../lib/progress-bar/progress-bar.component';
@@ -21,20 +27,27 @@ import { LineLoadingDirective } from '../../lib/loading/line-loading/line-loadin
 })
 export class PaymentLinesOverviewComponent {
   public loading = input.required<boolean>();
-  public paymentLinesOverviews = input.required<readonly GetPaymentLinesOverviewResponse[]>();
+  public paymentLinesOverviews =
+    input.required<readonly GetPaymentLinesOverviewResponse[]>();
 
-  protected readonly _allPaymentLinesOverviewWithName = computed<readonly GetPaymentLinesOverviewWithName[]>(() => {
+  protected readonly _allPaymentLinesOverviewWithName = computed<
+    readonly GetPaymentLinesOverviewWithName[]
+  >(() => {
     if (!this.paymentLinesOverviews()?.length) {
       return [];
     }
 
     return this.paymentLinesOverviews().map((paymentLineOverview) => ({
       ...paymentLineOverview,
-      name: subCategoryIdTranslationsMap.get(paymentLineOverview.subCategoryId) || '',
+      name:
+        subCategoryIdTranslationsMap.get(paymentLineOverview.subCategoryId) ||
+        '',
     }));
   });
 
-  protected readonly _paymentLinesOverviewWithName = computed<readonly GetPaymentLinesOverviewWithName[]>(() => {
+  protected readonly _paymentLinesOverviewWithName = computed<
+    readonly GetPaymentLinesOverviewWithName[]
+  >(() => {
     if (!this.paymentLinesOverviews()?.length) {
       return [];
     }
@@ -42,7 +55,9 @@ export class PaymentLinesOverviewComponent {
     const paymentLinesOverviewWithName: GetPaymentLinesOverviewWithName[] = [];
 
     for (let i = 0; i < this._numberOfItemsToShow(); i++) {
-      paymentLinesOverviewWithName.push(this._allPaymentLinesOverviewWithName()[i]);
+      paymentLinesOverviewWithName.push(
+        this._allPaymentLinesOverviewWithName()[i],
+      );
     }
 
     return paymentLinesOverviewWithName;
@@ -51,6 +66,8 @@ export class PaymentLinesOverviewComponent {
   protected readonly _seeMore = signal(false);
 
   protected _numberOfItemsToShow = computed<number>(() => {
-    return this._seeMore() ? this.paymentLinesOverviews().length : 3;
+    return this._seeMore()
+      ? this.paymentLinesOverviews().length
+      : Math.min(3, this.paymentLinesOverviews().length);
   });
 }
